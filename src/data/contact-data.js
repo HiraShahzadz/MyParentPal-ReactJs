89843c43b57534b8883b8caa0657d4770e789ae4
Hira Shahzad <hirashahzad565@gmail.com>
2023-10-14 19:17:53 +0500

    Change contact data and more parents data

--- src/data/contact-data.js
+++ src/data/contact-data.js
@@ -6,22 +6,50 @@ import {
 
 export const contactData = [
   {
-    title: "Excelent Services",
-    icon: StarIcon,
+    id: 1,
+    title: "Marium Yousaf",
     description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "This website is fantastic! I assigned multiple tasks to my 10 and 13-year-old children, and it has been incredibly helpful.",
+    image: "/img/women1.jpg",
   },
   {
-    title: "Grow Your Market",
+    id: 2,
+    title: "Ahmed Ali",
     icon: PresentationChartLineIcon,
     description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "I've been using this amazing website, and it's been quite helpful. I assigned a variety of tasks to my 7 year old chil. ",
+    image: "/img/men.jpg",
   },
   {
-    title: "Launch Time",
+    id: 3,
+    title: "Maham Sultan",
     icon: RocketLaunchIcon,
     description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "The tasks are engaging, making it an excellent resource for parents looking to keep their kids busy while learning.",
+    image: "/img/women2.jpg",
+  },
+  {
+    id: 4,
+    title: "Sham Shah",
+    description:
+      "This website is fantastic! I assigned multiple tasks to my 10 and 13-year-old children, and it has been incredibly helpful.",
+    image: "/img/Man5.jpg",
+  },
+  {
+    id: 5,
+    title: "Minahil Amir",
+    icon: PresentationChartLineIcon,
+    description:
+      "I've been using this amazing website, and it's been quite helpful. I assigned a variety of tasks to my 7 year old chil. ",
+    image: "/img/Woman6.jpg",
+  },
+  {
+    id: 6,
+    title: "Kamran Mustaq",
+    icon: PresentationChartLineIcon,
+    description:
+      "I've been using this amazing website, and it's been quite helpful. I assigned a variety of tasks to my 7 year old chil. ",
+    image: "/img/Man7.jpg",
   },
 ];
 
