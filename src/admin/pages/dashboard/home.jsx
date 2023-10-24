8cc50857f880398e84ab6f60d643a6e9dfdb9e3c
Nida Javed <nidajavedse@gmail.com>
2023-10-24 19:53:25 +0500

    Integrate admin into landing page

--- src/admin/pages/dashboard/home.jsx
+++ src/admin/pages/dashboard/home.jsx
@@ -19,14 +19,14 @@ import {
   EllipsisVerticalIcon,
   ArrowUpIcon,
 } from "@heroicons/react/24/outline";
-import { StatisticsCard } from "@/widgets/cards";
-import { StatisticsChart } from "@/widgets/charts";
+import { StatisticsCard } from "@/admin/widgets/cards";
+import { StatisticsChart } from "@/admin/widgets/charts";
 import {
   statisticsCardsData,
   statisticsChartsData,
   projectsTableData,
   ordersOverviewData,
-} from "@/data";
+} from "@/admin/data";
 
 export function Home() {
   return (
---
+++ src/admin/pages/dashboard/index.js
@@ -0,0 +1,4 @@
+export * from "@/admin/pages/dashboard/home";
+export * from "@/admin/pages/dashboard/profile";
+export * from "@/admin/pages/dashboard/tables";
+export * from "@/admin/pages/dashboard/notifications";
