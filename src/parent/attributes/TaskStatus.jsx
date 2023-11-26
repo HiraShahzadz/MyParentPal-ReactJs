f9c7fcb2cc543963242555212e6dcd8ee3c9fae6
Hira Shahzad <hirashahzad565@gmail.com>
2023-11-26 10:41:34 +0500

    Add task status and task time file. In this taking status of task and time for task submission.

---
+++ src/parent/attributes/TaskStatus.jsx
@@ -0,0 +1,79 @@
+import React, { Fragment, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const status = [
+  { label: "Todo", image: "/img/purple.png", color: "purple-50" },
+  { label: "In Progress", image: "/img/green.png", color: "green-50" },
+  { label: "Closed", image: "/img/orange.png", color: "deep-orange-50" },
+  { label: "Rewarded", image: "/img/blue.png", color: "blue-50" },
+];
+
+function TaskStatus() {
+  const [selectedStatus, setSelectedStatus] = useState(status[0]); // Initial selected status
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button
+          className={`inline-flex w-full justify-center gap-x-1.5 rounded-md bg-${selectedStatus.color} px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 `}
+        >
+          <img
+            className="m-1 h-3 w-3 rounded-full"
+            src={selectedStatus.image}
+            alt=""
+          />
+
+          {selectedStatus.label}
+          <ChevronDownIcon
+            className="-mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {status.map(({ label, image, color }, index) => (
+            <div className="py-1" key={index}>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    onClick={() => {
+                      setSelectedStatus({ label, image, color });
+                    }}
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "flex px-4 py-2 text-sm "
+                    )}
+                  >
+                    <img
+                      className="m-1 h-3 w-3 rounded-full"
+                      src={image}
+                      alt=""
+                    />
+                    <span className="ml-2">{label}</span>
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+
+export default TaskStatus;
