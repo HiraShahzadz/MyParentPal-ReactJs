787d231a3a08e7aa4fb684e35f39e576384193a6
Nida Javed <nidajavedse@gmail.com>
2023-12-06 12:15:52 +0500

    customize sign-up and sign-in

--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -10,40 +10,49 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import { SimpleFooter } from "@/widgets/layout";
+import "./Checkbox.css";
+import "./InputField.css";
 
 export function SignIn() {
   return (
     <>
       <img
-        src="/img/background-2.jpg"
+        src="https://wallpapers.com/images/hd/white-and-purple-m16ylro3bkdt9w0n.jpg"
         className="absolute inset-0 z-0 h-full w-full object-cover"
       />
-      <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
+
       <div className="container mx-auto p-4">
-        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
-          <CardHeader
-            variant="gradient"
-            color="blue"
-            className="mb-4 grid h-28 place-items-center"
-          >
+        <Card className="absolute left-2/4 top-2/4 w-full max-w-[24rem] -translate-x-2/4 -translate-y-2/4">
+          <CardHeader className="mb-4 grid h-20 place-items-center bg-MyPurple-400">
             <Typography variant="h3" color="white">
               Sign In
             </Typography>
           </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-            <Input variant="standard" type="email" label="Email" size="lg" />
+          <CardBody className="flex flex-col gap-4 ">
+            <Input
+              variant="standard"
+              type="email"
+              label="Email or Username"
+              size="lg"
+              className="focus:border-none"
+            />
+
             <Input
               variant="standard"
               type="password"
               label="Password"
               size="lg"
+              className="focus:border-MyPurple-400" // Add this class for focus styles
             />
-            <div className="-ml-2.5">
-              <Checkbox label="Remember Me" />
+
+            <div className="custom-checkbox">
+              <input type="checkbox" id="rememberMe" />
+              <label htmlFor="rememberMe">Remember me</label>
             </div>
           </CardBody>
+
           <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth>
+            <Button fullWidth className="bg-MyPurple-400">
               Sign In
             </Button>
             <Typography variant="small" className="mt-6 flex justify-center">
@@ -52,8 +61,7 @@ export function SignIn() {
                 <Typography
                   as="span"
                   variant="small"
-                  color="blue"
-                  className="ml-1 font-bold"
+                  className="ml-1 font-bold text-MyPurple-400"
                 >
                   Sign up
                 </Typography>
--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -15,38 +15,71 @@ export function SignUp() {
   return (
     <>
       <img
-        src="/img/background-2.jpg"
+        img
+        src="https://wallpapers.com/images/hd/white-and-purple-m16ylro3bkdt9w0n.jpg"
         className="absolute inset-0 z-0 h-full w-full object-cover"
       />
-      <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
+
       <div className="container mx-auto p-4">
-        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
+        <Card className="absolute left-2/4 top-2/4 w-full max-w-[24rem] -translate-x-2/4 -translate-y-2/4">
           <CardHeader
             variant="gradient"
-            color="blue"
-            className="mb-4 grid h-28 place-items-center"
+            className="mb-4 grid h-20 place-items-center bg-MyPurple-400"
           >
             <Typography variant="h3" color="white">
               Sign Up
             </Typography>
           </CardHeader>
           <CardBody className="flex flex-col gap-4">
-            <Input variant="standard" label="Name" size="lg" />
-            <Input variant="standard" type="email" label="Email" size="lg" />
+            <Input variant="standard" label="Email" type="Email" size="lg" />
             <Input
               variant="standard"
-              type="password"
+              type="Password"
               label="Password"
               size="lg"
             />
-            <div className="-ml-2.5">
-              <Checkbox label="I agree the Terms and Conditions" />
+            <Input
+              variant="standard"
+              type="password"
+              label="Confirm Password"
+              size="lg"
+            />
+
+            <div className="custom-checkbox">
+              <input type="checkbox" id="IagreetheTermsandConditions" />
+              <label htmlFor="IagreetheTermsandConditions">
+                I agree the Terms and Conditions
+              </label>
             </div>
           </CardBody>
           <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth>
+            <Button fullWidth className="bg-MyPurple-400">
               Sign Up
             </Button>
+            <br />
+            <Button
+              fullWidth
+              style={{
+                backgroundColor: "#FFFFFF",
+                color: "#B089BE",
+                border: "1px solid rgba(128, 128, 128, 0.5)",
+                display: "flex",
+                alignItems: "center", // Align items vertically in the center
+                justifyContent: "center", // Center content horizontally
+              }}
+            >
+              <img
+                src="/src/widgets/layout/google.svg"
+                alt="Google Icon"
+                style={{
+                  height: "20px", // Set a fixed height
+                  width: "20px", // Set a fixed width
+                  marginRight: "12px", // Add some spacing
+                }}
+              />
+              Sign In with Google
+            </Button>
+
             <Typography variant="small" className="mt-6 flex justify-center">
               Already have an account?
               <Link to="/sign-in">
@@ -54,7 +87,7 @@ export function SignUp() {
                   as="span"
                   variant="small"
                   color="blue"
-                  className="ml-1 font-bold"
+                  className="ml-1 font-bold text-MyPurple-400"
                 >
                   Sign in
                 </Typography>
