479ff508bfb09f3e030500ca0048c3ccb226a5b4
Hira Shahzad <hirashahzad565@gmail.com>
2023-12-08 21:13:34 +0500

    Add assignee, date and icons on drag and drop task modules.

--- src/parent/dragDrop/ListTasks.jsx
+++ src/parent/dragDrop/ListTasks.jsx
@@ -3,23 +3,23 @@ import Section from "@/parent/dragDrop/Section";
 
 function ListTasks({ tasks, setTasks }) {
   const [todos, setTodos] = useState([]);
-  const [inProgress, setInProgress] = useState([]);
-  const [closed, setClosed] = useState([]);
+  const [completed, setCompleted] = useState([]);
+  const [reviewed, setReviewed] = useState([]);
   const [rewarded, setRewarded] = useState([]);
 
   useEffect(() => {
     const fTodos = tasks?.filter((task) => task.status === "todo");
-    const fInProgress = tasks?.filter((task) => task.status === "inprogress");
-    const fClosed = tasks?.filter((task) => task.status === "closed");
+    const fCompleted = tasks?.filter((task) => task.status === "completed");
+    const fReviewed = tasks?.filter((task) => task.status === "reviewed");
     const fRewarded = tasks?.filter((task) => task.status === "rewarded");
 
     setTodos(fTodos);
-    setInProgress(fInProgress);
-    setClosed(fClosed);
+    setCompleted(fCompleted);
+    setReviewed(fReviewed);
     setRewarded(fRewarded);
   }, [tasks]);
 
-  const statuses = ["todo", "inprogress", "closed", "rewarded"];
+  const statuses = ["todo", "completed", "reviewed", "rewarded"];
 
   return (
     <div className="mb-12 grid gap-16 gap-x-6 gap-y-10 md:grid-cols-2 xl:grid-cols-4">
@@ -30,8 +30,8 @@ function ListTasks({ tasks, setTasks }) {
           tasks={tasks}
           setTasks={setTasks}
           todos={todos}
-          inProgress={inProgress}
-          closed={closed}
+          completed={completed}
+          reviewed={reviewed}
           rewarded={rewarded}
         />
       ))}
--- src/parent/dragDrop/Task.jsx
+++ src/parent/dragDrop/Task.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useDrag } from "react-dnd";
 import {
@@ -9,6 +9,7 @@ import {
   MenuItem,
 } from "@material-tailwind/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import EditTask from "./EditTask"; // Import your EditTask component
 
 function Task({ task, tasks, setTasks }) {
   const [{ isDragging }, drag] = useDrag(() => ({
@@ -19,8 +20,6 @@ function Task({ task, tasks, setTasks }) {
     }),
   }));
 
-  console.log(isDragging);
-
   const handleRemove = (id) => {
     const fTasks = tasks.filter((t) => t.id !== id);
 
@@ -29,31 +28,49 @@ function Task({ task, tasks, setTasks }) {
     toast("Task removed", { icon: "💀" });
   };
 
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div
       ref={drag}
-      className={`relative mt-8 cursor-grab rounded-md p-4 shadow-md ${
-        isDragging ? "opactiy-25" : "opactiy-100"
+      className={` relative mt-8 cursor-grab rounded-md p-4 shadow-md ${
+        isDragging ? "opacity-25" : "opacity-100"
       }`}
     >
-      <p>{task.name}</p>
-      <div className="absolute bottom-3 right-1">
-        <Menu placement="left-start">
-          <MenuHandler>
-            <IconButton size="sm" variant="text" color="blue-gray">
-              <EllipsisVerticalIcon
-                strokeWidth={3}
-                fill="currenColor"
-                className="h-6 w-6"
-              />
-            </IconButton>
-          </MenuHandler>
-          <MenuList>
-            <MenuItem onClick={() => handleRemove(task.id)}>Delete</MenuItem>
-            <div className="border-b-solid border-b border-b-gray-200"></div>
-            <MenuItem>Edit</MenuItem>
-          </MenuList>
-        </Menu>
+      <div className="flex">
+        <img className=" mr-2 mt-1 h-4 w-4 " src="/img/task.png" alt="" />
+        <p className="text-black hover:underline">{task.name}</p>
+        <div className="absolute right-1">
+          <Menu placement="left-start">
+            <MenuHandler>
+              <IconButton size="sm" variant="text" color="blue-gray">
+                <EllipsisVerticalIcon
+                  strokeWidth={2}
+                  fill="black"
+                  stroke="black"
+                  className="h-6 w-6"
+                />
+              </IconButton>
+            </MenuHandler>
+            <MenuList className="text-black">
+              <MenuItem onClick={() => handleRemove(task.id)}>Delete</MenuItem>
+              <div className="border-b border-gray-900 border-opacity-10"></div>
+              <MenuItem onClick={() => setShowModal(true)}>Edit</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+
+        {showModal && <EditTask task={task} onClose={setShowModal} />}
+      </div>
+      <div className="flex">
+        <p className="ml-0 mt-7 text-xs text-black ">5-2-2023</p>
+        <div className="absolute bottom-4 right-1">
+          <img
+            className="mr-2 mt-1 h-6 w-6 rounded-full"
+            src="/img/men.jpg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
