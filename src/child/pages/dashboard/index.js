f0c0381023298800036806ee62efee0e8aea35b6
Aiman Abid <aimanabid3002@gmail.com>
2023-12-26 13:43:31 +0500

    Remove signup path

--- src/child/pages/dashboard/index.js
+++ src/child/pages/dashboard/index.js
@@ -2,3 +2,5 @@ export * from "@/child/pages/dashboard/home";
 export * from "@/child/pages/dashboard/profile";
 export * from "@/child/pages/dashboard/request";
 export * from "@/child/pages/dashboard/notifications";
+export * from "@/child/pages/dashboard/ProgressReport";
+export * from "@/child/pages/dashboard/SubmitTask";
