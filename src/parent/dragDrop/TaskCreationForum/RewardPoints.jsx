1dbe6e2325983f7a75494f7fc9aa950787aa62e9
Hira Shahzad <hirashahzad565@gmail.com>
2023-12-27 16:51:11 +0500

    Add Edit task functionality and change in styling of create task and drag and drop section

--- src/parent/dragDrop/TaskCreationForum/RewardPoints.jsx
+++ src/parent/dragDrop/TaskCreationForum/RewardPoints.jsx
@@ -1,14 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
-const RewardPoints = () => {
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const handleOptionChange = (option) => {
-    setSelectedOption(option);
-  };
-
+const RewardPoints = (props) => {
   return (
-    <div className="mt-10 space-y-10 border-b border-gray-900/10 pb-6">
+    <div className="mt-5 space-y-10 border-b border-gray-900/10 pb-6">
       <fieldset>
         <legend className="flex text-sm font-semibold leading-6 text-gray-900">
           <svg
@@ -25,112 +19,37 @@ const RewardPoints = () => {
               d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
             />
           </svg>
-          Reward / Points
+          Reward
         </legend>
         <p className="mt-1 text-sm leading-6 text-gray-600">
-          Select to associate either reward or points to a task.
+          Associate either reward to a task.
         </p>
-        <div className="mt-5 flex gap-x-20 ">
-          <div className="flex items-center gap-x-3">
-            <input
-              id="reward"
-              name="reward-point"
-              type="radio"
-              className="h-4 w-4 border-gray-300 text-[#B089BE] focus:ring-[#B089BE]"
-              onChange={() => handleOptionChange("reward")}
-              checked={selectedOption === "reward"}
-            />
-            <label
-              htmlFor="push-everything"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Reward
-            </label>
-          </div>
-          <div className="flex items-center gap-x-3">
-            <input
-              id="points"
-              name="reward-point"
-              type="radio"
-              className="h-4 w-4 border-gray-300 text-[#B089BE] focus:ring-[#B089BE]"
-              onChange={() => handleOptionChange("points")}
-              checked={selectedOption === "points"}
-            />
-            <label
-              htmlFor="push-email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Points
-            </label>
-          </div>
-        </div>
-
-        {selectedOption === "points" && (
-          <div className="mt-4 sm:col-span-full">
+        <div className="mt-4 grid grid-cols-1 gap-x-6 sm:grid-cols-6">
+          <div className="sm:col-span-full">
             <label
               htmlFor="username"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Points
+              Reward
             </label>
             <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-[#B089BE] sm:max-w-md">
+              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-[#B089BE]">
                 <input
                   type="text"
-                  name="task"
-                  id="task"
-                  autoComplete="task"
+                  value={props.rewardname}
+                  onChange={(event) => {
+                    props.setRewardname(event.target.value);
+                  }}
+                  name="rewardname"
+                  id="rewardname"
+                  autoComplete="rewardname"
                   className="ml-1 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Total number of points"
+                  placeholder="Reward"
                 />
               </div>
             </div>
           </div>
-        )}
-
-        {selectedOption === "reward" && (
-          <div className="mt-4 grid grid-cols-1 gap-x-6 sm:grid-cols-6">
-            <div className="sm:col-span-full">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Reward
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-[#B089BE] sm:max-w-md">
-                  <input
-                    type="text"
-                    name="task"
-                    id="task"
-                    autoComplete="task"
-                    className="ml-1 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="Reward"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="col-span-full mt-4">
-              <label
-                htmlFor="about"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Reward Description
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="reward"
-                  name="reward"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B089BE] sm:text-sm sm:leading-6"
-                  defaultValue={""}
-                  placeholder="Write a few sentences about reward."
-                />
-              </div>
-            </div>
-          </div>
-        )}
+        </div>
       </fieldset>
     </div>
   );
--- src/parent/dragDrop/TaskCreationForum/TaskCreation.jsx
+++ src/parent/dragDrop/TaskCreationForum/TaskCreation.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { toast } from "react-hot-toast";
 import axios from "axios";
@@ -8,6 +8,9 @@ import {
   CardHeader,
   CardBody,
 } from "@material-tailwind/react";
+
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Toaster } from "react-hot-toast";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -18,9 +21,21 @@ import TaskTime from "../../attributes/TaskTime";
 import RewardPoints from "./RewardPoints";
 import MonitorTask from "./MonitorTask";
 import Tags from "@/parent/Report/Tags";
+import RegulateAccess from "./ RegulateAccess";
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
+const mystatus = [
+  { label: "Todo", image: "/img/purple.png" },
+  { label: "Completed", image: "/img/green.png" },
+  { label: "Reviewed", image: "/img/orange.png" },
+  { label: "Rewarded", image: "/img/blue.png" },
+];
 export function TaskCreation() {
-  const [tasks, setTasks] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState(mystatus[0]); // Initial selected status
+
+  const [tasks, setTasks] = useState(mystatus[0]);
 
   console.log("tasks", tasks);
 
@@ -72,22 +87,57 @@ export function TaskCreation() {
   const [taskid, setId] = useState("");
   const [taskname, setTaskname] = useState("");
   const [taskdescription, setTaskdescription] = useState("");
-
+  const [status, setStatus] = useState(mystatus[0].label);
+  const [rewardname, setRewardname] = useState("");
+  const [taskfiletype, setTaskfiletype] = useState([]);
+  const [taskdate, setTaskdate] = useState("");
+  const [tasktime, setTasktime] = useState("12:00");
+  const [tasktag, setTasktag] = useState("");
+  const [taskassignee, setTaskassignee] = useState("");
+  const [tasktype, setTasktype] = useState("");
+  console.log("setStatus value:", status);
+  //for saving in database
   async function save(event) {
     event.preventDefault();
     try {
-      await axios.post("http://localhost:8080/api/v1/task/save", {
-        taskname: taskname,
-        taskdescription: taskdescription,
-      });
-      alert("Task created Successfully");
+      if (taskname.length > 3 && taskname.length < 100) {
+        await axios.post("http://localhost:8080/api/v1/task/save", {
+          taskname: taskname,
+          taskdescription: taskdescription,
+          status: status,
+          rewardname: rewardname,
+          taskfiletype: taskfiletype,
+          taskdate: taskdate,
+          tasktime: tasktime,
+          tasktag: tasktag,
+          taskassignee: taskassignee,
+          tasktype: tasktype,
+        });
+        toast.success("Task Created");
 
-      setTaskname("");
-      setTaskdescription("");
+        setTaskname("");
+        setTaskdescription("");
+        setStatus("");
+        setRewardname("");
+        setTaskfiletype([]);
+        setTaskdate("");
+        setTasktime("");
+        setTasktag("");
+        setTaskassignee("");
+        setTasktype("");
+      }
+      if (taskname.length < 3) {
+        return toast.error("A task must have more than 3 characters");
+      }
+
+      if (taskname.length > 100) {
+        return toast.error("A task must not be more than 100 characters");
+      }
     } catch (err) {
-      alert("Task creation Failed");
+      return toast.error("Task creation is faild");
     }
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mt-9">
@@ -95,95 +145,90 @@ export function TaskCreation() {
           <Toaster />
         </DndProvider>
 
-        <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
+        <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3 ">
           <Card className="overflow-hidden xl:col-span-2">
-            <CardHeader
-              floated={false}
-              shadow={false}
-              color="transparent"
-              className="m-0 flex items-center justify-between p-6"
-            >
-              <div>
-                <Typography variant="h6" color="blue-gray" className="mb-1">
-                  Create Task
-                </Typography>
+            <CardBody className="overflow-x-scroll px-0 pb-3 pt-0">
+              <div className=" pb-2 pl-6 pt-6 text-left text-lg font-bold text-black">
+                Create Task
               </div>
-            </CardHeader>
 
-            <div className="px-6 pb-6">
-              <div className="space-y-5">
-                <div className="border-b border-gray-900/10 pb-7">
-                  <div className="grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
-                    <div className="sm:col-span-full">
-                      <label
-                        htmlFor="username"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Task
-                      </label>
-                      <div className="mt-2">
-                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-[#B089BE]">
-                          <input
+              <div className="px-6 pb-4">
+                <div className="space-y-5">
+                  <div className="border-b border-gray-900/10 pb-7">
+                    <div className="grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
+                      <div className="sm:col-span-full">
+                        <label
+                          htmlFor="username"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Task
+                        </label>
+                        <div className="mt-1">
+                          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-[#B089BE]">
+                            <input
+                              type="text"
+                              value={taskname}
+                              onChange={(event) => {
+                                setTaskname(event.target.value);
+                              }}
+                              name="taskname"
+                              id="taskname"
+                              autoComplete="taskname"
+                              className="ml-1 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                              placeholder="Task name"
+                            />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-span-full">
+                        <label
+                          htmlFor="about"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Description
+                        </label>
+                        <div className="mt-1">
+                          <textarea
                             type="text"
-                            value={taskname}
+                            id="taskdescription"
+                            name="taskdescription"
+                            value={taskdescription}
+                            rows={3}
                             onChange={(event) => {
-                              setTaskname(event.target.value);
+                              setTaskdescription(event.target.value);
                             }}
-                            name="taskname"
-                            id="taskname"
-                            autoComplete="taskname"
-                            className="ml-1 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                            placeholder="Task name"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B089BE] sm:text-sm sm:leading-6"
+                            placeholder="Write a few sentences about task."
                           />
                         </div>
                       </div>
                     </div>
-
-                    <div className="col-span-full">
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Description
-                      </label>
-                      <div className="mt-2">
-                        <textarea
-                          type="text"
-                          id="taskdescription"
-                          name="taskdescription"
-                          value={taskdescription}
-                          rows={3}
-                          onChange={(event) => {
-                            setTaskdescription(event.target.value);
-                          }}
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B089BE] sm:text-sm sm:leading-6"
-                          defaultValue={""}
-                          placeholder="Write a few sentences about task."
-                        />
-                      </div>
-                    </div>
+                  </div>
+                  <div>
+                    <RewardPoints
+                      rewardname={rewardname}
+                      setRewardname={setRewardname}
+                    />
+                  </div>
+                  <div>
+                    <MonitorTask
+                      taskfiletype={taskfiletype}
+                      setTaskfiletype={setTaskfiletype}
+                    />
+                  </div>
+                  <div>
+                    <RegulateAccess />
                   </div>
                 </div>
-                <div>
-                  <RewardPoints />
-                </div>
-                <div>
-                  <MonitorTask />
-                </div>
               </div>
-            </div>
+            </CardBody>
           </Card>
-          <Card>
-            <CardHeader
-              floated={false}
-              shadow={false}
-              color="transparent"
-              className="m-0 p-6"
-            >
-              <Typography variant="h6" color="blue-gray" className="mb-2">
-                Details
-              </Typography>
-            </CardHeader>
+          <Card className="h-fit">
+            <div className=" pb-2 pl-6 pt-6 text-left text-lg font-bold text-black">
+              Details
+            </div>
+
             <CardBody className="pt-0">
               <div className="flex">
                 <div className="mr-4 mt-2">
@@ -241,14 +286,17 @@ export function TaskCreation() {
                   </div>
 
                   <div className="mb-7">
-                    <TaskStatus />
+                    <TaskStatus setStatus={setStatus} />
                   </div>
 
                   <div className="mb-7">
-                    <CalenderInput />
+                    <CalenderInput
+                      taskdate={taskdate}
+                      setTaskdate={setTaskdate}
+                    />
                   </div>
                   <div className="mb-7">
-                    <TaskTime />
+                    <TaskTime tasktime={tasktime} setTasktime={setTasktime} />
                   </div>
                   <div>
                     <Tags />
@@ -295,13 +343,13 @@ export function TaskCreation() {
                 <button
                   type="submit"
                   onClick={save}
-                  className="mt-6 rounded-md bg-[#B089BE] px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-purple-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="mt-6 rounded-md bg-[#B089BE] px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                 >
                   + Create
                 </button>
                 <button
                   type="button"
-                  className="mt-6 rounded-md bg-gray-400 px-3 py-1 font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="mt-6 rounded-md bg-gray-400 px-4 py-1 font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
                 >
                   Cancel
                 </button>
