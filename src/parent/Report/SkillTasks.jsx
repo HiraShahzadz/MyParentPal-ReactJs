ff2b81a3f43ff77f1bd95b8c99a1d886f8d5c5b7
Hira Shahzad <hirashahzad565@gmail.com>
2023-12-27 16:33:26 +0500

    Add functionality of child progress against every skills

--- src/parent/Report/SkillTasks.jsx
+++ src/parent/Report/SkillTasks.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
-
+import { GiftIcon } from "@heroicons/react/24/solid";
 import tasksData from "@/parent/data/tasksData";
+import TaskDetailsModal from "../ChildProfiles/Profile/TaskDetailsModal";
 export function SkillTasks() {
+  const [taskDetailsToShow, setTaskDetailsToShow] = useState(null); //taskdetailmodel
+  const handleMoreInfoClick = (task) => {
+    setTaskDetailsToShow(task);
+  };
+  const handleCloseTaskDetails = () => {
+    setTaskDetailsToShow(null);
+  };
+
   return (
     <div className="ml-6 mr-6">
-      <div className="mb-1 mt-6 flex w-full items-center justify-between pl-3 pr-10">
+      <div className="mb-1 mt-3 flex w-full items-center justify-between pl-3 pr-10">
         <div className="text-left text-lg font-bold text-black">
           Task Summary
         </div>
@@ -20,17 +28,26 @@ export function SkillTasks() {
           </a>
         </div>
       </div>
-      <h2 className="text-md mb-3 ml-3 mt-5 font-bold">Assigned Tasks</h2>
+      <h2 className="text-md mb-3 ml-3 mt-3 font-bold">Assigned Tasks</h2>
       <div className="max-h-96">
         {tasksData.map(
-          ({ id, title, image, description, points, details, rewardImage }) => (
+          ({
+            id,
+            title,
+            image,
+            description,
+            points,
+            details,
+            rewardImage,
+            name,
+          }) => (
             <div
               key={id}
               href=""
-              className="mb-2 flex items-center rounded-md border p-3 text-sm hover:bg-blue-gray-50"
+              className="mb-2 items-center rounded-md border p-3 text-sm hover:bg-blue-gray-50 sm:flex"
             >
               <div className="flex">
-                <img className="mt-2 h-6 w-6 " src="/img/task.png" alt="" />
+                <img className="mt-0.5 h-6 w-6 " src="/img/task.png" alt="" />
                 <div className="ml-3">
                   <span className="font-medium text-black">{title}</span>
                   <br></br>
@@ -39,9 +56,20 @@ export function SkillTasks() {
                   </span>
                   <span className="text-black"></span>
                   <div className="mt-1.5 flex">
-                    <img className="h-3 w-3" src="/img/coin.png" alt="" />
-                    <span className="ml-1 text-xs text-black ">
+                    <GiftIcon className="h-4 w-4 rounded-sm text-MyPurple-400 " />
+
+                    <span className="ml-1 mt-0.5 text-xs text-black ">
                       Reward: {points}
+                    </span>
+                  </div>
+                  <div className="mt-1.5 flex">
+                    <img
+                      className="mt-0.5 h-5 w-5 rounded-s-full "
+                      src="/img/user.png"
+                      alt=""
+                    />
+                    <span className="ml-1 mt-1 text-xs text-black ">
+                      {name}
                     </span>
                   </div>
                 </div>
@@ -58,9 +86,10 @@ export function SkillTasks() {
                       points,
                       details,
                       rewardImage,
+                      name,
                     })
                   }
-                  className="mb-2 mt-14 rounded-lg bg-[#b089be] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-purple-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:mx-2 sm:mb-0"
+                  className=" mb-2 ml-8 mt-3 select-none rounded-lg border border-MyPurple-400 bg-white px-3 py-2 text-center align-middle font-sans text-sm font-semibold normal-case text-MyPurple-400 shadow-sm shadow-transparent transition-all hover:bg-MyPurple-400 hover:text-white hover:shadow-lg hover:shadow-white focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none md:rounded-md"
                 >
                   more info
                 </button>
@@ -87,14 +116,19 @@ export function SkillTasks() {
                 points,
                 details,
                 rewardImage,
+                name,
               }) => (
                 <div
                   key={id}
                   href=""
-                  className="mb-2 flex items-center rounded-md border p-3 text-sm hover:bg-blue-gray-50"
+                  className="mb-2 items-center rounded-md border p-3 text-sm hover:bg-blue-gray-50 sm:flex"
                 >
                   <div className="flex">
-                    <img className="mt-2 h-6 w-6 " src="/img/task.png" alt="" />
+                    <img
+                      className="mt-0.5 h-6 w-6 "
+                      src="/img/task.png"
+                      alt=""
+                    />
                     <div className="ml-3">
                       <span className="font-medium text-black">{title}</span>
                       <br></br>
@@ -103,9 +137,20 @@ export function SkillTasks() {
                       </span>
                       <span className="text-black"></span>
                       <div className="mt-1.5 flex">
-                        <img className="h-3 w-3" src="/img/coin.png" alt="" />
-                        <span className="ml-1 text-xs text-black ">
+                        <GiftIcon className="h-4 w-4 rounded-sm text-MyPurple-400 " />
+
+                        <span className="ml-1 mt-0.5 text-xs text-black ">
                           Reward: {points}
+                        </span>
+                      </div>
+                      <div className="mt-1.5 flex">
+                        <img
+                          className="mt-0.5 h-5 w-5 rounded-s-full "
+                          src="/img/user.png"
+                          alt=""
+                        />
+                        <span className="ml-1 mt-1 text-xs text-black ">
+                          {name}
                         </span>
                       </div>
                     </div>
@@ -122,9 +167,10 @@ export function SkillTasks() {
                           points,
                           details,
                           rewardImage,
+                          name,
                         })
                       }
-                      className="mb-2 mt-14 rounded-lg bg-[#b089be] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-purple-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:mx-2 sm:mb-0"
+                      className=" mb-2 ml-8 mt-3 select-none rounded-lg border border-MyPurple-400 bg-white px-3 py-2 text-center align-middle font-sans text-sm font-semibold normal-case text-MyPurple-400 shadow-sm shadow-transparent transition-all hover:bg-MyPurple-400 hover:text-white hover:shadow-lg hover:shadow-white focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none md:rounded-md"
                     >
                       more info
                     </button>
@@ -153,14 +199,19 @@ export function SkillTasks() {
                 points,
                 details,
                 rewardImage,
+                name,
               }) => (
                 <div
                   key={id}
                   href=""
-                  className="mb-2 flex items-center rounded-md border  p-3 text-sm hover:bg-blue-gray-50"
+                  className="mb-2 items-center rounded-md border p-3  text-sm hover:bg-blue-gray-50 sm:flex"
                 >
                   <div className="flex">
-                    <img className="mt-2 h-6 w-6 " src="/img/task.png" alt="" />
+                    <img
+                      className="mt-0.5 h-6 w-6 "
+                      src="/img/task.png"
+                      alt=""
+                    />
                     <div className="ml-3">
                       <span className="font-medium text-black">{title}</span>
                       <br></br>
@@ -169,9 +220,20 @@ export function SkillTasks() {
                       </span>
                       <span className="text-black"></span>
                       <div className="mt-1.5 flex">
-                        <img className="h-3 w-3" src="/img/coin.png" alt="" />
-                        <span className="ml-1 text-xs text-black ">
+                        <GiftIcon className="h-4 w-4 rounded-sm text-MyPurple-400 " />
+
+                        <span className="ml-1 mt-0.5 text-xs text-black ">
                           Reward: {points}
+                        </span>
+                      </div>
+                      <div className="mt-1.5 flex">
+                        <img
+                          className="mt-0.5 h-5 w-5 rounded-s-full "
+                          src="/img/user.png"
+                          alt=""
+                        />
+                        <span className="ml-1 mt-1 text-xs text-black ">
+                          {name}
                         </span>
                       </div>
                     </div>
@@ -188,9 +250,10 @@ export function SkillTasks() {
                           points,
                           details,
                           rewardImage,
+                          name,
                         })
                       }
-                      className="mb-2 mt-14 rounded-lg bg-[#b089be] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-purple-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:mx-2 sm:mb-0"
+                      className=" mb-2 ml-8 mt-3 select-none rounded-lg border border-MyPurple-400 bg-white px-3 py-2 text-center align-middle font-sans text-sm font-semibold normal-case text-MyPurple-400 shadow-sm shadow-transparent transition-all hover:bg-MyPurple-400 hover:text-white hover:shadow-lg hover:shadow-white focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none md:rounded-md"
                     >
                       more info
                     </button>
@@ -199,6 +262,13 @@ export function SkillTasks() {
               )
             )}
         </div>
+        {taskDetailsToShow && (
+          <TaskDetailsModal
+            selectedTaskDetails={taskDetailsToShow}
+            handleCloseTaskDetails={handleCloseTaskDetails}
+            // handleSubmitTask={/* Pass your handleSubmitTask function here */}
+          />
+        )}
       </div>
     </div>
   );
