3b5d1c08e6eb4cbb8657f6e670470f326cc182e7
Hira Shahzad <hirashahzad565@gmail.com>
2023-12-27 16:40:01 +0500

    Add data files

---
+++ src/parent/data/RespondNotificationsData.js
@@ -0,0 +1,88 @@
+const RespondNotificationsData = [
+  {
+    time: "11:00",
+    name: "Hira Shahzad",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Cell Phone",
+  },
+  {
+    time: "01:00",
+    name: "Nida Javed",
+    description: "requested for a task",
+    image: "/img/user.png",
+    task: "Laptop",
+  },
+  {
+    time: "1 Week",
+    name: "Aiman Abid",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Play Ground",
+  },
+  {
+    time: "1 Week",
+    name: "Ali Bhati",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Cell Phone",
+  },
+  {
+    time: "2 Week",
+    name: "Minahil Raja",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Ice Cream",
+  },
+  {
+    time: "2 Week",
+    name: "Ali Bhati",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Play Station",
+  },
+  {
+    time: "1 year",
+    name: "Ali Bhati",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Pen",
+  },
+  {
+    time: "1 year",
+    name: "Nida Javed",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Bag",
+  },
+  {
+    time: "1 year",
+    name: "Aiman Abid",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Shoes",
+  },
+  {
+    time: "1 year",
+    name: "Hira Shahzad",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Colours",
+  },
+  {
+    time: "1 year",
+    name: "Nida Javed",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Toys",
+  },
+  {
+    time: "1 year",
+    name: "Hira Shahzad",
+    description: "requested for a reward",
+    image: "/img/user.png",
+    task: "Play Ground",
+  },
+];
+
+export default RespondNotificationsData;
