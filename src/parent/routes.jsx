1eba6f602b9603cfca6742ec8288ae3388e235f4
Hira Shahzad <hirashahzad565@gmail.com>
2023-12-27 16:58:05 +0500

    Add child report and Evaluate task routs

--- src/parent/routes.jsx
+++ src/parent/routes.jsx
@@ -1,21 +1,27 @@
 import {
   HomeIcon,
   UserCircleIcon,
-  TableCellsIcon,
   BellIcon,
-  ArrowRightOnRectangleIcon,
+  ArrowLeftOnRectangleIcon,
+  ChartBarSquareIcon,
+  ClipboardDocumentCheckIcon,
+  PlusIcon,
+  UserGroupIcon,
   UserPlusIcon,
-  RectangleGroupIcon,
-  RectangleStackIcon,
-  UserIcon,
 } from "@heroicons/react/24/solid";
 
-import { ParentHome, Tables, Notifications } from "@/parent/pages/dashboard";
+import {
+  ParentHome,
+  ChildReport,
+  Notifications,
+} from "@/parent/pages/dashboard";
 import { SignIn, SignUp } from "@/parent/pages/auth";
 import TaskCreation from "./dragDrop/TaskCreationForum/TaskCreation";
 import MyChild from "./ChildProfiles/MyChild";
 import { ParentProfile } from "./ParentProfile/parentProfile";
 import { MyProfile } from "./ChildProfiles/Profile/profile";
+import { TaskEvaluate } from "./EvaluateTask/TaskEvaluate";
+import TaskRequests from "./requests/TaskRequests";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -38,30 +44,50 @@ export const routes = [
         element: <ParentProfile />,
       },
       {
-        icon: <RectangleGroupIcon {...icon} />,
+        icon: <UserGroupIcon {...icon} />,
+        name: "My Child",
+        path: "/child",
+        element: <MyChild />,
+      },
+      {
+        icon: (
+          <PlusIcon
+            {...icon}
+            className="h-4 rounded-sm border bg-white text-blue-gray-800"
+            stroke="currentColor"
+            strokeWidth="1"
+          />
+        ),
         name: "create tasks",
         path: "/tasks",
         element: <TaskCreation />,
       },
       {
-        icon: <UserIcon {...icon} />,
-        name: "My Child",
-        path: "/child",
-        element: <MyChild />,
+        icon: <ClipboardDocumentCheckIcon {...icon} />,
+        name: "Evaluate Task",
+        path: "/evaluation",
+        element: <TaskEvaluate />,
       },
 
       {
         icon: <BellIcon {...icon} />,
-        name: "notifactions",
-        path: "/notifactions",
+        name: "Notifications",
+        path: "/notifications",
         element: <Notifications />,
       },
 
       {
-        icon: <TableCellsIcon {...icon} />,
-        name: "tables",
-        path: "/tables",
-        element: <Tables />,
+        icon: <UserPlusIcon {...icon} />,
+        name: "Task Requests",
+        path: "/requests",
+        element: <TaskRequests />,
+      },
+
+      {
+        icon: <ChartBarSquareIcon {...icon} />,
+        name: "Child Report",
+        path: "/report",
+        element: <ChildReport />,
       },
     ],
   },
@@ -70,16 +96,10 @@ export const routes = [
     layout: "auth",
     pages: [
       {
-        icon: <ArrowRightOnRectangleIcon {...icon} />,
-        name: "sign in",
+        icon: <ArrowLeftOnRectangleIcon {...icon} />,
+        name: "Logout",
         path: "/sign-in",
         element: <SignIn />,
-      },
-      {
-        icon: <UserPlusIcon {...icon} />,
-        name: "sign up",
-        path: "/sign-up",
-        element: <SignUp />,
       },
     ],
   },
