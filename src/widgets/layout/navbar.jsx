c9f6499ee0dd3e0773e4c3b269ff1b3c33496430
Hira Shahzad <hirashahzad565@gmail.com>
2024-01-16 21:13:14 +0500

    Change in style

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -72,24 +72,7 @@ export function Navbar({ brandName, routes, action }) {
           </Typography>
         </Link>
         <div className="hidden lg:block">{navList}</div>
-        <div className="hidden gap-2 lg:flex">
-          <a
-            href="https://www.material-tailwind.com/blocks?ref=mtkr"
-            target="_blank"
-          >
-            <Button
-              variant="text"
-              size="sm"
-              style={{ color: "#b089bf" }}
-              fullWidth
-            >
-              Sign In
-            </Button>
-          </a>
-          {React.cloneElement(action, {
-            className: "hidden lg:inline-block",
-          })}
-        </div>
+        <div className="hidden gap-2 lg:flex"></div>
         <IconButton
           variant="text"
           size="sm"
