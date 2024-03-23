ab57dc80526675bcafc594b52a5a3d3a09c2f2ce
Hira Shahzad <hirashahzad565@gmail.com>
2024-03-23 11:43:27 +0500

    Display task detail,edit task details and save it in to database

--- src/parent/dragDrop/Task.jsx
+++ src/parent/dragDrop/Task.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { useDrag } from "react-dnd";
 import {
@@ -11,7 +11,7 @@ import {
 import axios from "axios";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import EditTask from "./EditTask"; // Import your EditTask component
-import tasksData from "@/parent/data/tasksData";
+
 function Task({ task, tasks, setTasks }) {
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "task",
@@ -31,7 +31,7 @@ function Task({ task, tasks, setTasks }) {
 
   const [showModal, setShowModal] = useState(false);
   const [taskid, setId] = useState("");
-  async function DeleteStudent(taskid) {
+  async function DeleteTask(taskid) {
     await axios.delete("http://localhost:8081/api/v1/task/delete/" + taskid);
     toast("Task removed", { icon: "💀" });
 
@@ -45,6 +45,20 @@ function Task({ task, tasks, setTasks }) {
   const handleCloseTaskDetails = () => {
     setTaskDetailsToShow(null);
   };
+  const [childProfileData, setChildProfileData] = useState([]);
+
+  useEffect(() => {
+    (async () => await Load())();
+  }, []);
+
+  async function Load() {
+    const result = await axios.get(
+      "http://localhost:8081/api/v1/user/get-child"
+    );
+    setChildProfileData(result.data);
+    console.log(result.data);
+  }
+
   return (
     <div
       ref={drag}
@@ -68,9 +82,7 @@ function Task({ task, tasks, setTasks }) {
               </IconButton>
             </MenuHandler>
             <MenuList className="text-black">
-              <MenuItem onClick={() => DeleteStudent(task._id)}>
-                Delete
-              </MenuItem>
+              <MenuItem onClick={() => DeleteTask(task._id)}>Delete</MenuItem>
               <div className="border-b border-gray-900 border-opacity-10"></div>
 
               <MenuItem
@@ -93,6 +105,7 @@ function Task({ task, tasks, setTasks }) {
 
         {taskDetailsToShow && (
           <EditTask
+            task={task}
             selectedTaskDetails={taskDetailsToShow}
             handleCloseTaskDetails={handleCloseTaskDetails}
           />
@@ -101,11 +114,21 @@ function Task({ task, tasks, setTasks }) {
       <div className="flex">
         <p className="ml-0 mt-7 text-xs text-black ">{task.taskdate}</p>
         <div className="absolute bottom-4 right-1">
-          <img
-            className="mr-2 mt-1 h-6 w-6 rounded-full"
-            src="/img/userc.png"
-            alt=""
-          />
+          {childProfileData.map(({ id, img, role }, index) => {
+            // Assuming task is defined and childId is accessible
+            if (id === task.childId) {
+              return (
+                <img
+                  key={id} // Ensure each element in the array has a unique key
+                  className="mr-2 mt-1 h-6 w-6 rounded-full object-cover"
+                  src={img ? `data:image/jpeg;base64,${img}` : "/img/userc.png"}
+                  alt=""
+                />
+              );
+            } else {
+              return null; // If the id doesn't match, return null
+            }
+          })}
         </div>
       </div>
     </div>
