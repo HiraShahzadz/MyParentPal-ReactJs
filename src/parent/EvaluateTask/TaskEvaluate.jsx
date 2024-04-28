3116f49c104744ecc25dddb2e35666ac4d2e74ec
Hira Shahzad <hirashahzad565@gmail.com>
2024-04-28 09:02:07 +0500

    API integration in evaluate task. Create Penalty task. Add evaluat task details in penalty task with some additional details and update some fields of evaluate task

--- src/parent/EvaluateTask/TaskEvaluate.jsx
+++ src/parent/EvaluateTask/TaskEvaluate.jsx
@@ -1,16 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Card, CardBody } from "@material-tailwind/react";
 import { GiftIcon } from "@heroicons/react/24/solid";
-import tasksData from "@/parent/data/tasksData";
-import PaneltyTask from "./PaneltyTask";
+
+import PenaltyTask from "./PenaltyTask";
 import { Toaster } from "react-hot-toast";
 import { DndProvider } from "react-dnd";
 import { toast } from "react-hot-toast";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Tags from "../Report/Tags";
 import ChildSelection from "../Report/ChildSelection";
+
 export function TaskEvaluate() {
+  const [childProfileData, setChildProfileData] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [tasksData, setTasksData] = useState([]);
+  const [selectedChildId, setSelectedChildId] = useState(null);
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  useEffect(() => {
+    loadChildProfileData();
+    loadTasks();
+  }, []);
+
+  async function loadChildProfileData() {
+    try {
+      const result = await axios.get(
+        "http://localhost:8081/api/v1/user/get-child"
+      );
+      setChildProfileData(result.data);
+      console.log("Child profile data:", result.data);
+    } catch (error) {
+      console.error("Error loading child profile data:", error);
+    }
+  }
+
+  async function loadTasks() {
+    try {
+      const result = await axios.get(
+        "http://localhost:8081/api/v1/task/getall"
+      );
+      setTasksData(result.data);
+      console.log("All tasks:", result.data);
+    } catch (error) {
+      console.error("Error loading tasks:", error);
+    }
+  }
+
+  // Filter tasks based on child IDs
+  const filteredTasks = tasksData.filter(
+    (task) =>
+      childProfileData.some((child) => child.id === task.childId) &&
+      (selectedChildId ? task.childId === selectedChildId : true)
+  );
+
+  const filterTasksByChild = (childId) => {
+    setSelectedChildId(childId);
+  };
+
+  const handleEvaluateTask = (task) => {
+    setSelectedTask(task);
+    setShowModal(true);
+  };
+
   return (
     <div className="mb-4 mt-9 flex flex-col gap-12">
       <DndProvider backend={HTML5Backend}>
@@ -26,7 +78,11 @@ export function TaskEvaluate() {
           <div className="mt-4 flex">
             <Tags />
             <div className="ml-3">
-              <ChildSelection />
+              <ChildSelection
+                childProfileData={childProfileData}
+                setChildProfileData={setChildProfileData}
+                filterTasks={filterTasksByChild}
+              />
             </div>
           </div>
         </div>
@@ -37,74 +93,78 @@ export function TaskEvaluate() {
                 Completed Tasks
               </h2>
               <div className="max-h-96">
-                {tasksData.map(
-                  ({
-                    id,
-                    title,
-                    image,
-                    description,
-                    points,
-                    details,
-                    rewardImage,
-                    name,
-                  }) => (
-                    <div
-                      key={id}
-                      href=""
-                      className="mb-2 items-center rounded-md border p-3 text-sm hover:bg-blue-gray-50 sm:flex"
-                    >
-                      <div className="flex">
-                        <img
-                          className="mt-0.5 h-6 w-6 "
-                          src="/img/task.png"
-                          alt=""
-                        />
-                        <div className="ml-3">
-                          <span className="font-medium text-black">
-                            {title}
-                          </span>
-                          <br></br>
-                          <span className="mt-2 text-black">
-                            Submission date: {description.toLocaleDateString()}
-                          </span>
-                          <span className="text-black"></span>
-                          <div className="mt-1.5 flex">
-                            <GiftIcon className="h-4 w-4 rounded-sm text-MyPurple-400 " />
+                {filteredTasks.map((task, index) => (
+                  <div
+                    key={task.id}
+                    href=""
+                    className="mb-2 items-center rounded-md border p-3 text-sm hover:bg-blue-gray-50 sm:flex"
+                  >
+                    <div className="flex">
+                      <img
+                        className="mt-0.5 h-6 w-6 "
+                        src="/img/task.png"
+                        alt=""
+                      />
+                      <div className="ml-3">
+                        <span className="font-medium text-black">
+                          {task.taskname}
+                        </span>
+                        <br></br>
+                        <span className="mt-2 text-black">
+                          Submission date: {task.taskdate}
+                        </span>
+                        <span className="text-black"></span>
+                        <div className="mt-1.5 flex">
+                          <GiftIcon className="h-4 w-4 rounded-sm text-MyPurple-400 " />
 
-                            <span className="ml-1 mt-0.5 text-xs text-black ">
-                              Reward: {points}
-                            </span>
-                          </div>
-                          <div className="mt-1.5 flex">
-                            <img
-                              className="mt-0.5 h-5 w-5 rounded-s-full "
-                              src="/img/user.png"
-                              alt=""
-                            />
-                            <span className="ml-1 mt-1 text-xs text-black ">
-                              {name}
-                            </span>
-                          </div>
+                          <span className="ml-1 mt-0.5 text-xs text-black ">
+                            Reward: {task.rewardname}
+                          </span>
+                        </div>
+                        <div className="mt-1.5 flex">
+                          {childProfileData.map(
+                            (child) =>
+                              child.id === task.childId && (
+                                <img
+                                  className="mt-0.5 h-5 w-5 rounded-full object-cover"
+                                  src={
+                                    child.img
+                                      ? `data:image/jpeg;base64,${child.img}`
+                                      : "/img/userc.png"
+                                  }
+                                  alt=""
+                                />
+                              )
+                          )}
+                          <span className="ml-1 mt-1 text-xs text-black ">
+                            {task.taskassignee}
+                          </span>
                         </div>
                       </div>
-
-                      <div className="ml-auto flex items-end  hover:border-MyPurple-400">
-                        <button
-                          onClick={() => setShowModal(true)}
-                          className=" mb-2 ml-8 mt-3 select-none rounded-lg border border-MyPurple-400 bg-white px-3 py-2 text-center align-middle font-sans text-sm font-semibold normal-case text-MyPurple-400 shadow-sm shadow-transparent transition-all hover:bg-MyPurple-400 hover:text-white hover:shadow-lg hover:shadow-white focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none md:rounded-md"
-                        >
-                          Evaluate now
-                        </button>
-                      </div>
-                      {showModal && <PaneltyTask onClose={setShowModal} />}
                     </div>
-                  )
-                )}
+
+                    <div className="ml-auto flex items-end  hover:border-MyPurple-400">
+                      <button
+                        onClick={() => handleEvaluateTask(task)}
+                        className=" mb-2 ml-8 mt-3 select-none rounded-lg border border-MyPurple-400 bg-white px-3 py-2 text-center align-middle font-sans text-sm font-semibold normal-case text-MyPurple-400 shadow-sm shadow-transparent transition-all hover:bg-MyPurple-400 hover:text-white hover:shadow-lg hover:shadow-white focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none md:rounded-md"
+                      >
+                        Evaluate now
+                      </button>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </CardBody>
       </Card>
+      {showModal && (
+        <PenaltyTask
+          onClose={setShowModal}
+          task={selectedTask}
+          childProfileData={childProfileData}
+        />
+      )}
     </div>
   );
 }
--- src/parent/Report/ChildSelection.jsx
+++ src/parent/Report/ChildSelection.jsx
@@ -5,25 +5,33 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const options = [
-  { label: "Select Child", image: "/img/user.png" },
-  { label: "Nida", image: "/img/userc.png" },
-  { label: "Hira", image: "/img/userc.png" },
-];
-
-function ChildSelection() {
-  const [selectedStatus, setSelectedStatus] = useState(options[0]); // Initial selected status
-
+function ChildSelection({
+  childProfileData,
+  setChildProfileData,
+  filterTasks,
+}) {
+  const [selectedStatus, setSelectedStatus] = useState(
+    childProfileData.length > 0
+      ? childProfileData[0]
+      : { img: "", name: "Select Child" }
+  );
+  const handleChildClick = (childId) => {
+    filterTasks(childId);
+  };
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           <img
-            className="h-5 w-5 rounded-full"
-            src={selectedStatus.image}
+            className="h-5 w-5 rounded-full  object-cover"
+            src={
+              selectedStatus.img
+                ? `data:image/jpeg;base64,${selectedStatus.img}`
+                : "/img/userc.png"
+            }
             alt=""
           />
-          {selectedStatus.label}
+          {selectedStatus.name}
           <ChevronDownIcon
             className="-mr-1 h-5 w-5 text-gray-400"
             aria-hidden="true"
@@ -41,21 +49,27 @@ function ChildSelection() {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          {options.map(({ label, image }, index) => (
-            <div className="py-1" key={index}>
-              <Menu.Item>
+          {childProfileData.map(({ name, img, id }) => (
+            <div className="py-1" key={id}>
+              <Menu.Item onClick={() => handleChildClick(id)}>
                 {({ active }) => (
                   <a
                     onClick={() => {
-                      setSelectedStatus({ label, image });
+                      setSelectedStatus({ id, name, img });
                     }}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                       "flex px-4 py-2 text-sm "
                     )}
                   >
-                    <img className="h-6 w-6 rounded-full" src={image} alt="" />
-                    <span className="ml-2">{label}</span>
+                    <img
+                      className="h-6 w-6 rounded-full object-cover"
+                      src={
+                        img ? `data:image/jpeg;base64,${img}` : "/img/userc.png"
+                      }
+                      alt=""
+                    />
+                    <span className="ml-2">{name}</span>
                   </a>
                 )}
               </Menu.Item>
