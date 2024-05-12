c7b8d6e276fa24923d6b0b403a36a84e018d6110
Hira Shahzad <hirashahzad565@gmail.com>
2024-05-12 14:31:58 +0500

    Add Reassign Task fuctionality

---
+++ src/parent/dragDrop/ReassignTasks.jsx
@@ -0,0 +1,294 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import CalenderInput from "../attributes/CalenderInput";
+import TaskTime from "../attributes/TaskTime";
+import { Toaster } from "react-hot-toast";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+function ReassignTasks({ task, selectedTaskDetails, handleCloseTaskDetails }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDetails, setEditedDetails] = useState({
+    title: task.taskname,
+    details: task.taskdescription,
+    reward: task.rewardname,
+    fileType: task.taskfiletype,
+    tags: task.tasktag,
+    taststatus: "Todo",
+    assignee: task.taskassignee,
+    type: task.tasktype,
+    myChildId: task.childId,
+  });
+  const [editedDate, setEditedDate] = useState(new Date(task.taskdate));
+  const defaultFileTypes = task.taskfiletype
+    ? task.taskfiletype.map((item) => item.trim())
+    : [];
+  const [fileTypes, setFileTypes] = useState(defaultFileTypes);
+  const [savedDetails, setSavedDetails] = useState(null);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+  const [taskdate, setTaskdate] = useState(task.taskdate);
+  const [tasktime, setTasktime] = useState(task.tasktime);
+  async function reassign(event) {
+    event.preventDefault();
+    if (
+      !editedDetails.title ||
+      !editedDetails.details ||
+      !editedDetails.reward ||
+      !taskdate ||
+      !tasktime
+    ) {
+      return toast.error("Please fill in all fields");
+    }
+
+    const taskDate = new Date(taskdate);
+    const currentDate = new Date();
+
+    // Extract the date part without considering the time
+    const taskDateWithoutTime = new Date(taskDate.toDateString());
+    const currentDateWithoutTime = new Date(currentDate.toDateString());
+
+    if (taskDateWithoutTime < currentDateWithoutTime) {
+      return toast.error("Task submission date cannot be in the past ");
+    }
+    const selectedTime = new Date(taskdate + " " + tasktime);
+
+    // Compare with the current time
+    const currentTime = new Date();
+
+    if (selectedTime <= currentTime) {
+      return toast.error(
+        "Task submission time cannot be in the past or present (1-24)"
+      );
+    }
+
+    try {
+      if (editedDetails.title.length > 3 && editedDetails.title.length < 15) {
+        let url1 = "http://localhost:8081/api/v1/task/reassign";
+        let url2 = "http://localhost:8081/api/v1/notify/assigntaskNotification";
+
+        let promise1 = axios.post(url1, {
+          taskname: editedDetails.title,
+          taskdescription: editedDetails.details,
+          rewardname: editedDetails.reward,
+          status: editedDetails.taststatus,
+          taskdate: taskdate,
+          tasktime: tasktime,
+          tasktag: editedDetails.tags,
+          taskfiletype: fileTypes,
+          taskassignee: editedDetails.assignee,
+          tasktype: editedDetails.type,
+          childId: editedDetails.myChildId,
+        });
+
+        let promise2 = axios.post(url2, {
+          taskname: editedDetails.title,
+        });
+        Promise.all([promise1, promise2]);
+        toast.success("Task Reassigned");
+        // Update the saved details state with the edited details
+        setSavedDetails({
+          taskname: editedDetails.title,
+          taskdescription: editedDetails.details,
+          rewardname: editedDetails.reward,
+          status: editedDetails.taststatus,
+          taskdate: taskdate,
+          tasktime: tasktime,
+          tasktag: editedDetails.tags,
+          taskassignee: editedDetails.assignee,
+          tasktype: editedDetails.type,
+          childId: editedDetails.myChildId,
+          taskfiletype: fileTypes.join(", "),
+        });
+        // Switch back to non-editable mode after saving
+        setIsEditing(false);
+        // Reload the page
+        window.location.reload();
+      } else if (editedDetails.title.length <= 3) {
+        return toast.error("A task must have more than 3 characters");
+      } else if (editedDetails.title.length >= 15) {
+        return toast.error("A task must not be more than 15 characters");
+      }
+    } catch (err) {
+      return toast.error("Task creation is failed", err);
+    }
+  }
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedDetails({ ...selectedTaskDetails });
+    setEditedDate(new Date(selectedTaskDetails.description));
+    setFileTypes(defaultFileTypes);
+  };
+
+  const handleInputChange = (field, value) => {
+    setEditedDetails({ ...editedDetails, [field]: value });
+  };
+
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    let updatedFileTypes = [...fileTypes];
+    if (checked) {
+      updatedFileTypes.push(value);
+    } else {
+      updatedFileTypes = updatedFileTypes.filter((type) => type !== value);
+    }
+    setFileTypes(updatedFileTypes);
+  };
+
+  const renderEditableField = (field, placeholder, label) => {
+    return (
+      <div className="mb-6">
+        <DndProvider backend={HTML5Backend}>
+          <Toaster />
+        </DndProvider>
+        <label
+          htmlFor={field}
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          {label}
+        </label>
+        <input
+          type="text"
+          id={field}
+          name={field}
+          value={editedDetails[field]}
+          onChange={(e) => handleInputChange(field, e.target.value)}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B089BE] sm:text-sm sm:leading-6"
+          placeholder={placeholder}
+          required
+        />
+      </div>
+    );
+  };
+
+  const renderFileTypeField = () => {
+    return (
+      <div className="mb-6">
+        <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+          Submission File Type
+        </label>
+        <div>
+          <label className="mr-4 inline-flex items-center">
+            <input
+              type="checkbox"
+              value="Picture"
+              checked={fileTypes.includes("Picture")}
+              onChange={handleCheckboxChange}
+              className="border-gray-300 text-MyPurple-400 focus:ring-MyPurple-400"
+            />
+            <span className="ml-2">Picture</span>
+          </label>
+          <label className="mr-4 inline-flex items-center">
+            <input
+              type="checkbox"
+              value="Video"
+              checked={fileTypes.includes("Video")}
+              onChange={handleCheckboxChange}
+              className="border-gray-300 text-MyPurple-400 focus:ring-MyPurple-400"
+            />
+            <span className="ml-2">Video</span>
+          </label>
+          <label className="mr-4 inline-flex items-center">
+            <input
+              type="checkbox"
+              value="Audio"
+              checked={fileTypes.includes("Audio")}
+              onChange={handleCheckboxChange}
+              className="border-gray-300 text-MyPurple-400 focus:ring-MyPurple-400"
+            />
+            <span className="ml-2">Audio</span>
+          </label>
+          <label className="mr-4 inline-flex items-center">
+            <input
+              type="checkbox"
+              value="Text"
+              checked={fileTypes.includes("Text")}
+              onChange={handleCheckboxChange}
+              className="border-gray-300 text-MyPurple-400 focus:ring-MyPurple-400"
+            />
+            <span className="ml-2">Text</span>
+          </label>
+        </div>
+        <input
+          type="text"
+          value={fileTypes.join(", ")}
+          readOnly={true}
+          placeholder="File Type"
+          className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B089BE] sm:text-sm sm:leading-6"
+          disabled={!isEditing}
+          required
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50">
+      <div className="w-[600px] rounded-lg bg-white p-6 shadow-lg">
+        <div className="flex justify-end">
+          <button
+            className="focus:outline-none"
+            onClick={handleCloseTaskDetails}
+          >
+            <FontAwesomeIcon icon={faTimes} className="text-lg text-gray-600" />
+          </button>
+        </div>
+
+        <p variant="h5" className="center mb-4 text-justify text-sm text-black">
+          {renderEditableField("title", "Enter Task Name", "Task Name")}
+          {renderEditableField("details", "Enter Description", "Description")}
+          {renderEditableField("reward", "Enter Reward", "Reward")}
+          {renderFileTypeField()}
+          <div className="flex">
+            <div className="mt-3">
+              <label
+                htmlFor="about"
+                className="mb-2 block text-sm font-medium leading-6 text-gray-900"
+              >
+                Submission Date
+              </label>
+              <CalenderInput taskdate={taskdate} setTaskdate={setTaskdate} />
+            </div>
+            <div className="ml-10 mt-3">
+              <label
+                htmlFor="about"
+                className="mb-2 block text-sm font-medium leading-6 text-gray-900"
+              >
+                Submission Time
+              </label>
+              <TaskTime tasktime={tasktime} setTasktime={setTasktime} />
+            </div>
+          </div>
+        </p>
+
+        <div className="flex justify-end">
+          <div className="mt-2 flex">
+            <button
+              type="submit"
+              onClick={reassign}
+              className="mr-2 rounded-md bg-MyPurple-400 px-4 py-2 text-sm font-semibold normal-case text-white shadow-sm shadow-white hover:bg-purple-400 hover:shadow-white"
+            >
+              Save
+            </button>
+            <button
+              onClick={handleCloseTaskDetails}
+              className="rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold normal-case text-white shadow-sm shadow-white hover:bg-gray-500 hover:shadow-white"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ReassignTasks;
