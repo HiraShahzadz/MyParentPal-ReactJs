b3a642d280c3dbbbe53ca3bb64c5cf180123326e
Hira Shahzad <hirashahzad565@gmail.com>
2024-05-21 22:28:12 +0500

    Add and resolve child side issues

--- src/child/pages/dashboard/notifications.jsx
+++ src/child/pages/dashboard/notifications.jsx
@@ -139,52 +139,51 @@ export function Notifications() {
           </div>
         </CardHeader>
         <CardBody className="flex flex-col gap-4 p-4">
-          {myProfile && (
-            <>
-              {requestrs
-                .sort((a, b) => new Date(b.date) - new Date(a.date))
-                .filter((Notification) => Notification.childId === myProfile.id)
-                .filter(({ message }) => message.startsWith("Your parent"))
-                .map(({ ChildName, message, taskname, time }, index) => (
-                  <div
-                    className="flex items-center rounded-md p-3 text-sm hover:bg-blue-gray-50"
-                    key={index}
-                  >
-                    <div className="flex">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src="/img/userc.png"
-                        alt=""
-                      />
-                      <div className="ml-3">
-                        <span className="text-black">{message}</span>
-                        <span className="text-neutral-400 ml-2 mt-2 text-gray-400">
-                          {time}
+          {requestrs.length > 0
+            ? requestrs.map(({ ChildName, message, taskname, time }, index) => (
+                <div
+                  className="flex items-center rounded-md p-3 text-sm hover:bg-blue-gray-50"
+                  key={index}
+                >
+                  <div className="flex">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src="/img/userc.png"
+                      alt=""
+                    />
+                    <div className="ml-3">
+                      <span className="text-black">{message}</span>
+                      <span className="text-neutral-400 ml-2 mt-2 text-gray-400">
+                        {time}
+                      </span>
+                      <div className="mt-1.5 flex">
+                        <img className="h-3 w-3" src="/img/task.png" alt="" />
+                        <span className="ml-1 text-xs text-black hover:underline">
+                          {taskname}
                         </span>
-                        <div className="mt-1.5 flex">
-                          <img className="h-3 w-3" src="/img/task.png" alt="" />
-                          <span className="ml-1 text-xs text-black hover:underline">
-                            {taskname}
-                          </span>
-                        </div>
                       </div>
                     </div>
-                    {!hiddenImages.includes(index) && (
-                      <div
-                        className="ml-auto flex items-end rounded-full border p-1 hover:border-MyPurple-400"
-                        onClick={() => handleImageClick(index)}
-                      >
-                        <img
-                          className="h-1.5 w-1.5 rounded-full"
-                          src="/img/purple.png"
-                          alt=""
-                        />
-                      </div>
-                    )}
                   </div>
-                ))}
-            </>
-          )}
+                  {!hiddenImages.includes(index) && (
+                    <div
+                      className="ml-auto flex items-end rounded-full border p-1 hover:border-MyPurple-400"
+                      onClick={() => handleImageClick(index)}
+                    >
+                      <img
+                        className="h-1.5 w-1.5 rounded-full"
+                        src="/img/purple.png"
+                        alt=""
+                      />
+                    </div>
+                  )}
+                </div>
+              ))
+            : myProfile && (
+                <Typography color="gray">
+                  Your notifications are scheduled between {myProfile.startTime}{" "}
+                  and {myProfile.endTime}.
+                </Typography>
+              )}
         </CardBody>
       </Card>
     </div>
